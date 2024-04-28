5e8d079ca4b6cc5ea4b6ecd308de62767b79ba5d
bayuhusada <nekobay08@gmail.com>
2024-04-28 21:27:28 +0700

    FE-ubah chart

--- index.php
+++ index.php
@@ -23,15 +23,12 @@
 
           <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
+                <h6 class="m-0 font-weight-bold text-danger">Area Chart</h6>
             </div>
             <div class="card-body">
                 <div class="chart-area">
                     <canvas id="myAreaChart"></canvas>
                 </div>
-                <hr>
-                Styling for the area chart can be found in the
-                <code>/js/demo/chart-area-demo.js</code> file.
             </div>
         </div>
 
