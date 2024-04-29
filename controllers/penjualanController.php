d916b4d827e6feff06d3a3c23dc06ef7f8853fb1
riothung <mario.thung@gmail.com>
2024-04-30 00:21:15 +0800

    add penjualan & penjualanController

---
+++ controllers/penjualanController.php
@@ -0,0 +1,97 @@
+<?php 
+
+session_start();
+
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+
+require '../db.php';
+$conn = OpenCon();
+
+// echo json_encode($_POST);
+
+switch ($action) {
+    case 'add':
+        if(isset($_POST['submit'])){
+            $tanggal = $_POST['nama_bahan'];
+            $harga = $_POST['harga'];
+            $stok = $_POST['stok'];
+
+            $sql = "INSERT INTO bahan (nama_bahan, harga, stok) VALUES ('$tanggal', '$harga', '$stok')";
+
+            try {
+                $result = $conn->query($sql);
+                $_SESSION['success-alert'] = 'Berhasil menambah data';
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+                exit();
+            }catch(PDOException $e){
+                $_SESSION['failed-alert'] = 'Gagal menambah data';
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+                exit();
+            }
+        }
+            $conn->close();
+            break;
+
+        case 'edit':
+            if(isset($_POST['submit'])){
+
+            $id = $_REQUEST['id'];                
+            
+            $tanggal = $_POST['nama_bahan'];
+            $harga = $_POST['harga'];
+            $stok = $_POST['stok'];
+
+            $sql = "UPDATE bahan SET nama_bahan = '$tanggal', harga = '$harga', stok = '$stok' WHERE id = '$id'";
+
+            // echo json_encode($_POST);
+
+            try{
+                $result = $conn->query($sql);
+                $_SESSION['success-alert'] = 'Berhasil merubah data';
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+                exit();
+            }catch(PDOException $e){
+                $_SESSION['failed-alert'] = 'Gagal merubah data';
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+                exit();
+            }
+        }
+            $conn->close();
+            break;
+
+            case 'delete':
+
+                $id = $_REQUEST['id'];
+                
+                $tanggal = $_POST['nama_bahan'];
+                $harga = $_POST['harga'];
+                $stok = $_POST['stok'];
+                
+                $sql = "DELETE FROM bahan WHERE id = '$id'";
+
+                try{
+                    $result = $conn->query($sql);
+                    $_SESSION['success-alert'] = 'Berhasil menghapus data';
+                    header("Location: " . $_SERVER['HTTP_REFERER']);
+                    exit();
+                }catch(PDOException $e){
+                    $_SESSION['failed-alert'] = 'Gagal menghapus data';
+                    header("Location: " . $_SERVER['HTTP_REFERER']);
+                    exit();
+            }
+            $conn -> close();
+            break;
+    }
+
+
+
+        
+
+
+
+
+
+
+
+
+?>
