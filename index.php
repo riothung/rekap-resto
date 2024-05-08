e83d771cf1f03e296ece09dda2d8ea867afc732f
riothung <mario.thung@gmail.com>
2024-05-09 03:15:44 +0800

    fix typo statistik

--- index.php
+++ index.php
@@ -197,7 +197,7 @@ $labelsJSON = json_encode($labels);
           <!-- cart start -->
           <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-danger">Gafik Penjualan</h6>
+                <h6 class="m-0 font-weight-bold text-danger">Grafik Penjualan</h6>
             </div>
             <div class="card-body">
                 <div class="chart-area">
